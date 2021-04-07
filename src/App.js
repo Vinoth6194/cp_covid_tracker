9cffe5495fb876e9293e26d09b5b72b8da308b57
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-07 18:40:50 +0530

    classname fix

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
       <div className="app__header">
         <h1>Covid-19 Tracker</h1>
 
-        <FormControl className="app_dropdown">
+        <FormControl className="app__dropdown">
           <Select variant="outlined" value={country} onChange={onCountryChange}>
             <MenuItem value="worldwide">WorldWide</MenuItem>
             {countries.map(country => (
