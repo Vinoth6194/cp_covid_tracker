13da68fdb867183eb38dd4543dc46d238b980688
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-07 17:09:08 +0530

    countries dropdowwn from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import { FormControl, MenuItem, Select } from '@material-ui/core';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [countries, setCountries] = useState(['USA', 'UK', 'India']);
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then(response => response.json())
+        .then(data => {
+          const countries = data.map(country => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
   return (
     <div className="app">
       <div class="app__header">
@@ -12,7 +27,7 @@ function App() {
         <FormControl className="app_dropdown">
           <Select variant="outlined" value="abc">
             {countries.map(country => (
-              <MenuItem value={country}>{country}</MenuItem>
+              <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
           </Select>
         </FormControl>
