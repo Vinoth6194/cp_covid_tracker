949cc59f02c8273dabc030c5568265ccc056f239
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-07 16:17:20 +0530

    dummydropdown

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { FormControl, MenuItem, Select } from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>Covid19 Tracker</h1>
+      <h1>Covid-19 Tracker</h1>
+
+      <FormControl className="app_dropdown">
+        <Select variant="outlined" value="abc">
+          <MenuItem value="worldwide">worldwide</MenuItem>
+          <MenuItem value="worldwide">Option 2</MenuItem>
+          <MenuItem value="worldwide">option 3</MenuItem>
+          <MenuItem value="worldwide">hey</MenuItem>
+        </Select>
+      </FormControl>
 
       {/* header */}
       {/* title + country dropdown */}
